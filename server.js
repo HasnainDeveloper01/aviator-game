c1ff4bee9c5a581b8bf3426e7fb910998d6e2094
HasnainDeveloper01 <hasnainjamal0011@gmail.com>
2025-08-15 12:21:12 +0500

    Fixed static file serving for image/plane.svg

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/image', express.static(__dirname + '/image'));
 
 const SECRET_KEY = process.env.SECRET_KEY || 'asdfghjqwerty';
 
